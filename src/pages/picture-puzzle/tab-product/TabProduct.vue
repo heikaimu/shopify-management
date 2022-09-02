b517ad5d22328d571d0a522330cf55562a933c35
Liu YaoWen <576079353@qq.com>
2022-09-02 09:28:30 +0800

    1

---
+++ src/pages/picture-puzzle/tab-product/TabProduct.vue
@@ -0,0 +1,19 @@
+<!--
+ * @Date: 2022-08-30 16:54:58
+ * @LastEditors: Yaowen Liu
+ * @LastEditTime: 2022-08-30 16:55:44
+ * @FilePath: /shopify-management/src/pages/picture-puzzle/tab-product/TabProduct.vue
+-->
+<template>
+  <div>
+    dwa
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
